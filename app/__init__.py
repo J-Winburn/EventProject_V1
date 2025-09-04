d8359fcf7189ea6cde7bb96acd6298e78cf662b9
Jared Winburn <jaredwinburn@MacBook-Pro.local>
2025-09-04 15:21:01 -0400

    Added midels, routes, run, and config files. Basic Setup

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+
+    @app.route("/")   # temp route just to test
+    def home():
+        return "Hello, Flask! 🚀"
+
+    return app
